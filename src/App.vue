4523693c1637160a98c86e037476d23d40b3a030
jaume-serra <jaumeserraa@gmail.com>
2022-06-15 13:32:28 +0200

    Add main component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
-
 import Header from './components/Header.vue'
+import Home from './components/Home.vue'
+
+
 
 </script>
 
 <template>
   <Header msg="Hello Vue 3 + TypeScript + Vite" />
+  <Home></Home>
 </template>
 
 <style>
---
+++ src/components/Home.vue
@@ -0,0 +1,49 @@
+<script  lang="ts">
+import { ref } from 'vue'
+import { defineComponent } from 'vue'
+
+
+ export default defineComponent({
+  // type inference enabled
+//   props: {
+//     name: String,
+//     msg: { type: String, required: true }
+//   },
+  data(){
+    return {
+      loading:true,
+      title: 'Global',
+      date: '',
+      stats: {},
+      country: [],
+    }
+  },
+  methods: {
+    async fetchCovidData() {
+      const result = await fetch('https://api.covid19api.com/summary')
+      const dataCovid = await result.json()
+      return dataCovid
+    }
+  },
+  async created (){
+    const data = await this.fetchCovidData()
+    this.date = data.Date
+    this.stats = data.Global
+    this.country= data.Countries
+    this.loading = false
+
+}
+})
+
+
+</script>
+
+<template>
+    <main>
+        hola
+    </main>
+</template>
+
+<style scoped>
+
+</style>
